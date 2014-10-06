c91515e981f966d4a7ee6c5e159d0f07456234be
Megh <skyspeak@gmail.com>
2014-10-05 19:47:17 -0700

    Tip calculator
    Tip Calculator app as a prereq for the iOS course

--- tipapp/ViewController.swift
+++ tipapp/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        tipLabel.text="0.00"
+        totalLabel.text="0.00"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +26,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        println("User editing")
+        var tipPercentages = [0.15,0.20,0.25]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        
+        var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        tipLabel.text = "\(tip)"
+        totalLabel.text = "\(total)"
+        
+        tipLabel.text = String(format:"$%.2f" ,tip)
+        totalLabel.text = String(format:"$%.2f" ,total)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
